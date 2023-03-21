bf65c690e2ce6bd0b9304cd8e981f5fdb3a24847
Preisler <preisler.andras@gmail.com>
2023-03-21 10:14:19 +0100

    Update server.js
    main

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/api/posts', (req, res) => {
-    res.json(
-        {
-            posts: [{id: 1,title: 'Post 1',description: 'Content 1'}, {id: 2,title: 'Post 2',description: 'Content 2'}, {id: 3,title: 'Post 3',description: 'Content 3'},]
-        }
-    )
-})
+app.get("/api/posts", (req, res) => {
+  res.json({
+    posts: [
+        { id: 1, title: "Post 1", description: "Content 1" },
+        { id: 2, title: "Post 2", description: "Content 2" },
+        { id: 3, title: "Post 3", description: "Content 3" },
+        { id: 4, title: "Post 4", description: "Content 4" },
+        { id: 5, title: "Post 5", description: "Content 5" },
+    ],
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
