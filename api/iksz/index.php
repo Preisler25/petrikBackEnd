70c40b6d8d222e0db17b86d91ba6a0be70646fd2
Preisler <preisler.andras@gmail.com>
2023-03-24 21:41:59 +0100

    main

--- api/iksz/index.php
+++ api/iksz/index.php
@@ -6,7 +6,18 @@
         "image" => "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
     );
 
+    $obj2 = array(
+        "id" => 2,
+        "title" => "alma",
+        "description" => "adsadasdasdasda",
+        "image" => "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+    );
+
+    $obj3 = array(
+        "posts" => array($obj, $obj2)
+    );
+
     header('Content-Type: application/json');
-    echo json_encode($obj);
+    echo json_encode($obj3);
 
 ?>
