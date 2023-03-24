d6ca4153a4f0ec88443d9ed6b853b060122faa10
Preisler <preisler.andras@gmail.com>
2023-03-24 23:41:26 +0100

    main

--- api/login/index.php
+++ api/login/index.php
@@ -2,28 +2,4 @@
     require '../db.php';
 
     echo($_GET["username"]);
-
-    // Az adatbáziskapcsolat használata
-    $conn = OpenCon();
-    
-    // Adatok lekérdezése a táblából
-    $sql = "SELECT id, name, email FROM users";
-    $result = $conn->query($sql);
-    
-    // Eredmények megjelenítése
-    if ($result->num_rows > 0) {
-        while($row = $result->fetch_assoc()) {
-            echo "ID: " . $row["id"]. " - Name: " . $row["name"]. " - Email: " . $row["email"]. "<br>";
-        }
-    } else {
-        echo "Nincs eredmény.";
-    }
-    
-    // Az adatbáziskapcsolat lezárása
-    CloseCon($conn);
-
-    $user = array(
-        "username" => $_POST["username"],
-        "password" => $_POST["password"],
-    );
->
+?>
