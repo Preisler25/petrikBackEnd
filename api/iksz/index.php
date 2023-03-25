747490bfb902726456ca8f2a37bd96f4b07d2b27
Preisler <preisler.andras@gmail.com>
2023-03-25 21:27:20 +0100

    main

--- api/iksz/index.php
+++ api/iksz/index.php
@@ -14,10 +14,17 @@
     );
 
     $obj3 = array(
-        "posts" => array($obj, $obj2)
+        "id" => 3,
+        "title" => "szilva",
+        "description" => "adsadasdasdasda",
+        "image" => "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+    );
+
+    $obj4 = array(
+        "posts" => array($obj, $obj2, $obj3)
     );
 
     header('Content-Type: application/json');
-    echo json_encode($obj3);
+    echo json_encode($obj4);
 
 ?>
--- ikszadmin/index.php
+++ ikszadmin/index.php
@@ -4,12 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="../css/style.css">
     <title>Document</title>
 </head>
 <body>
 <div class="login">
-        <form action="./api/iksz/index.php" method="POST">
+        <form action="../api/iksz/index.php" method="POST">
             <input type="number" name="id" placeholder="ID">
             <input type="text" name="title" placeholder="Cím">
             <input type="text" name="description" placeholder="Leírás">
