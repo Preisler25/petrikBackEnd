36566844c31788dc35d0094d93e370ab4ead81f9
Preisler <preisler.andras@gmail.com>
2023-03-26 12:35:45 +0200

    fixing iksz regiszt

--- api/regiksz/index.php
+++ api/regiksz/index.php
@@ -9,9 +9,34 @@
     $img_url = $_POST['image'];
 
     
-    $sql = "INSERT INTO `iksz` (`max`, `title`, `description`, `img_url`) VALUES ( '$max', '$title', '$description', '$img_url');";
+    $sql = "SELECT * FROM `iksz` WHERE `title` = '$title'";
 
     $res = $conn->query($sql);
 
-    echo $res;
+    if ($res->num_rows > 0) {
+        echo "Van már ilyen című iksz";
+        die();
+    }
+    else{
+        $sql_ins = "INSERT INTO `iksz` (`max`, `title`, `description`, `img_url`) VALUES ( '$max', '$title', '$description', '$img_url');";
+
+        $res_ins = $conn->query($sql_ins);
+
+        if ($res_ins) {
+            $sql_table = "Create table $title (user_id int(11))";
+            $res_table = $conn->query($sql_table);
+            if ($res_table) {
+                header("Location: http://localhost/ikszadmin");
+                die();
+            }
+            else{
+                echo "table create error";
+                die();
+            }
+        }
+        else{
+            echo "Sikertelen Insert";
+            die();
+        }
+    }    
 ?>
