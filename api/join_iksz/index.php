7bfea7ff68d9450256349ab6c349765bf1b41b2c
Preisler <preisler.andras@gmail.com>
2023-03-28 12:06:55 +0200

    MAIN

--- api/join_iksz/index.php
+++ api/join_iksz/index.php
@@ -5,5 +5,58 @@
     $conn = OpenCon();
 
     $name = $_POST['name'];
+    $key = $_POST['key'];
 
+    $sql = "Select * from user where name = '$name'";
+    $res = $conn->query($sql);
+
+    if($res->num_rows > 0){
+        $row = $res->fetch_assoc();
+        $user_id = $row['id'];
+        $user_osztaly = $row['osztaly'];
+        $user_name = $row['name'];
+
+        $valid = password_verify($user_osztaly, $key);
+
+        $ik = $_POST['ik'];
+    
+        if($valid){
+    
+            $sql = "Select * from user where name = '$name'";
+            $result = $conn->query($sql);
+    
+            if($result->num_rows > 0){
+                $row = $result->fetch_assoc();
+                $user_id = $row['id'];
+    
+    
+                $sql = "Insert into $ik (user_id) values ('$user_id')";
+                $result = $conn->query($sql);
+
+                if($result){
+                    $user = array(
+                        'name' => $user_name,
+                        'osztaly' => $user_osztaly,
+                    );
+        
+                    $obj = array('status' => TRUE, 'user' => $user);
+        
+                    header('Content-Type: application/json');
+                    echo json_encode($obj);
+                }else{
+                    $user = array(
+                        'name' => '',
+                        'osztaly' => '',
+                    );
+                    $obj = array(
+                    'status' => FALSE,
+                    'user' => $user,
+                );
+        
+                    header('Content-Type: application/json');
+                    echo json_encode($obj);
+                }
+            }
+        }
+    }
 ?>
