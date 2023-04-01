b648a0fc2e28aee70ee7e4b514cc58e8d3930253
Preisler <preisler.andras@gmail.com>
2023-04-01 17:11:25 +0200

    main

--- api/login/index.php
+++ api/login/index.php
@@ -1,46 +1,53 @@
 <?php
-    require_once '../../util/db.php';
-    require_once '../../util/logLogic.php';
+require_once '../../util/db.php';
+require_once '../../util/logLogic.php';
 
-    $conn = OpenCon();
+$conn = OpenCon();
 
-     // Geting data from Get
-     $name = $_GET['name'];
-     $password = $_GET['password'];
+// Geting data from Get
+$name = $_GET['name'];
+$password = $_GET['password'];
 
-    $valid = chPass($conn, $name, $password);
-    //main
-    if($valid){
-        $sql = "SELECT * FROM user WHERE name = '$name'";
-        $result = $conn->query($sql);
+// validating password
+include_once '../../util/passwordValidation.php';
+if(!validatePassword($password)){
+    $user = array(
+        'name' => '',
+        'osztaly' => '',
+        'key' => '',
+        'fullname' => '',
+    );
+    $obj = array('status' => FALSE, 'user' => $user);
+    header('Content-Type: application/json');
+    echo json_encode($obj);
+    exit();
+}
 
-        if($result->num_rows > 0){
+$valid = chPass($conn, $name, $password);
 
-            $row = $result->fetch_assoc();
+//main
+if($valid){
+    //prepared statement to prevent SQL injection
+    $stmt = $conn->prepare("SELECT * FROM user WHERE name = ?");
+    $stmt->bind_param("s", $name);
+    $stmt->execute();
 
-            $key = password_hash($row['email'], PASSWORD_DEFAULT);
+    $result = $stmt->get_result();
+    if($result->num_rows > 0){
 
-            $user = array(
-                'name' => $row['name'],
-                'osztaly' => $row['osztaly'],
-                'key' => $key,
-                'fullname' => $row['fullname'],
-            );
+        $row = $result->fetch_assoc();
 
-            $obj = array('status' => TRUE, 'user' => $user);
-        }else{
+        $key = password_hash($row['email'], PASSWORD_DEFAULT);
 
-            $user = array(
-                'name' => '',
-                'osztaly' => '',
-                'key' => '',
-                'fullname' => '',
-            );
+        $user = array(
+            'name' => $row['name'],
+            'osztaly' => $row['osztaly'],
+            'key' => $key,
+            'fullname' => $row['fullname'],
+        );
 
-            $obj = array('status' => FALSE, 'user' => $user);
-        }
-    }
-    else{
+        $obj = array('status' => TRUE, 'user' => $user);
+    }else{
 
         $user = array(
             'name' => '',
@@ -51,9 +58,21 @@
 
         $obj = array('status' => FALSE, 'user' => $user);
     }
-    
-    header('Content-Type: application/json');
-    echo json_encode($obj);
+}
+else{
 
-    CloseCon($conn);
+    $user = array(
+        'name' => '',
+        'osztaly' => '',
+        'key' => '',
+        'fullname' => '',
+    );
+
+    $obj = array('status' => FALSE, 'user' => $user);
+}
+
+header('Content-Type: application/json');
+echo json_encode($obj);
+
+CloseCon($conn);
 ?>
